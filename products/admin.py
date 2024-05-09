3c9e1c61b6b95c424c5f1395774f074944ab1bb0
ZTZ <tzamirov@gmail.com>
2024-05-09 23:09:27 +0600

    create models for products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+
+    list_display = ("name", "slug", "parent_category")
+    list_display_links = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Товары"""
+
+    list_display = ("name", "slug", "price", "quantity")
+    list_display_links = ("name",)
+    readonly_fields = ("get_image",)
+    prepopulated_fields = {"slug": ("name",)}
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="100"')
+
+    get_image.short_description = "Изображение"
--- products/models.py
+++ products/models.py
@@ -1,3 +1,63 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+def product_image_path(instance, filename):
+    return f"mediafiles/products/{instance.slug}/{filename}"
+
+
+class Category(models.Model):
+    name = models.CharField(
+        ("Категория"),
+        max_length=100,
+    )
+    parent_category = models.ForeignKey(
+        "self",
+        related_name="Подкатегории",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    slug = models.SlugField(
+        ("Slug"),
+        max_length=100,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(("Название"), max_length=100)
+    description = models.TextField(("Описание"), blank=True)
+    image = models.ImageField(("Изображение"), upload_to=product_image_path, blank=True)
+    price = models.DecimalField(("Цена"), decimal_places=2, max_digits=10)
+    quantity = models.IntegerField(default=1)
+    category = models.ForeignKey(
+        Category,
+        related_name="product_list",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    slug = models.SlugField(
+        ("Slug"),
+        max_length=100,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
