132b2dcf51d61e20b8eeb6f92356ef907b3b7602
ZTZ <tzamirov@gmail.com>
2024-05-11 14:11:42 +0600

    add users permissions, views, urls

--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,18 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import AddressViewSet, UserRegisterView, UserRetrieveView
+
 
 app_name = "users"
 
 router = DefaultRouter()
+router.register(r"", AddressViewSet, basename="addresses")
 
 urlpatterns = [
+    path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", UserRetrieveView.as_view(), name="user_detail"),
+    path("me/address/", include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import *
+from .permissions import IsUserAddressOwner
+
+
+class UserRegisterView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        user = self.request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserRetrieveView(RetrieveAPIView):
+    """
+    Get user details
+    """
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+
+class AddressViewSet(ReadOnlyModelViewSet):
+    """
+    List and Retrieve user addresses
+    """
+
+    permission_classes = (IsUserAddressOwner,)
+    serializer_class = AddressReadOnlySerializer
+
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user)
